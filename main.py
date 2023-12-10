29cff8bd8c7bdd82f1cca31c9a24004d2c91241f
Lighted <6216563-Lighted@users.noreply.replit.com>
2023-12-10 12:17:15 +0000

    initial git before sharing to the others

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# When running the app for the first time, run this command to activate the css stuff
+# npx tailwindcss -i ./static/src/input.css -o ./static/css/output.css --watch
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+	return render_template('home.html')
+
+
+@app.route('/login')
+def login():
+	return render_template('login.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=81, debug=True)
+	app.run(host='0.0.0.0', port=81, debug=True)
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./templates/**/*.{html,js}"],
+	theme: {
+		extend: {
+			colors: {
+				'proj-white': '#EEEEEE',
+				'proj-blue': '#00ADB5',
+				'proj-gray': '#393E46',
+				'proj-black': '#222831',
+			}
+		},
+	},
+	plugins: [],
+}
+
