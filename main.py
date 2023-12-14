f51e07d64435c3780cfde55aa0190b1ccba8fbcd
Lighted <6216563-Lighted@users.noreply.replit.com>
2023-12-14 14:53:11 +0000

    added stuff

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
+from subprocess import Popen
 
+DEBUG = True
 app = Flask(__name__)
 
 # When running the app for the first time, run this command to activate the css stuff
@@ -7,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template('home.html')
+	return render_template('home.html', blend_nav=True)
 
 
 @app.route('/login')
@@ -15,5 +17,22 @@ def login():
 	return render_template('login.html')
 
 
+@app.route('/book')
+def book():
+	return render_template('booking.html')
+	
+
+@app.route('/tracker')
+def tracker():
+	return render_template('tracker.html')
+
+
+@app.route('/manage')
+def manage():
+	return render_template('manage.html')
+
+
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=81, debug=True)
+	if DEBUG:
+		Popen('npx tailwindcss -i ./static/src/input.css -o ./static/css/output.css --watch', shell=True)
+	app.run(host='0.0.0.0', port=81, debug=DEBUG)
